df08d3f8ec536d3b04ea9f3d18164f7d89aee1fd
LinoSun <913896141@qq.com>
2019-07-01 15:40:25 +0800

    移除元素

---
+++ daily/27.移除元素.py
@@ -0,0 +1,51 @@
+'''
+给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+示例 1:
+
+给定 nums = [3,2,2,3], val = 3,
+
+函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+
+你不需要考虑数组中超出新长度后面的元素。
+示例 2:
+
+给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+
+函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+
+注意这五个元素可为任意顺序。
+
+你不需要考虑数组中超出新长度后面的元素。
+'''
+
+# python语言特性做
+class Solution:
+    def removeElement(self, nums: [int], val: int) -> int:
+        i = 0
+
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+
+        return len(nums)
+
+# 第二种做法（很垃圾）
+class Solution:
+    def removeElement(self, nums: [int], val: int) -> int:
+        j = 0
+        if not nums:
+            return 0
+
+        for v in nums:
+            if v != val:
+                nums[j] = v
+                j += 1
+
+        return j
