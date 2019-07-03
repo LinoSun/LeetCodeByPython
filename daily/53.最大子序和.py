f7b661de1051bbf8aeb1d3cba46785e96a422117
LinoSun <913896141@qq.com>
2019-07-03 18:07:32 +0800

    dp经典 最大子序和

--- daily/53.最大子序和.py
+++ daily/53.最大子序和.py
@@ -9,9 +9,25 @@
 '''
 
 #  review
-nums = [-2,1,-3,4,-1,2,1,-5,4]
+# 最简洁做法，也是状态转换点
+class Solution:
+    def maxSubArray(self, nums: [int]) -> int:
+        for i in range(1,len(nums)):
+            nums[i] = max(nums[i-1] + nums[i], nums[i])
+        return max(nums)
 
-for i in range(1,len(nums)):
-    nums[i] = max(nums[i-1] + nums[i], nums[i])
 
-print(max(nums))
+
+# 比较容易理解的动态规划，但是状态转换点不理解
+class Solution:
+    def maxSubArray(self, nums: [int]) -> int:
+        if max(nums) < 0:
+            return max(nums)
+
+        local_max, global_max = 0, 0
+
+        for num in nums:
+            local_max = max(0, local_max + num)
+            global_max = max(local_max, global_max)
+
+        return global_max
