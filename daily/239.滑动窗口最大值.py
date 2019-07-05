42cee4c656946fd518d6306f306746c511a99bf9
LinoSun <913896141@qq.com>
2019-07-05 14:02:07 +0800

    回看代码

---
+++ daily/239.滑动窗口最大值.py
@@ -0,0 +1,35 @@
+'''
+给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口 k 内的数字。滑动窗口每次只向右移动一位。
+
+返回滑动窗口最大值。
+
+示例:
+
+输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+输出: [3,3,5,5,6,7]
+解释:
+
+  滑动窗口的位置                最大值
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+'''
+
+# 笨方法，回看题解
+class Solution:
+    def maxSlidingWindow(self, nums: [int], k: int) -> [int]:
+        result = []
+        if not nums:
+            return result
+
+        if len(nums) <= k:
+            return [max(nums)]
+
+        for i in range(len(nums) - k + 1):
+            result.append(max(nums[i:i + k]))
+
+        return result
