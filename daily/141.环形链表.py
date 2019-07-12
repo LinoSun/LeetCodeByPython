148daf88eb66d6f8779bb917398061d721aafec9
LinoSun <913896141@qq.com>
2019-07-12 17:34:48 +0800

    链表有环

---
+++ daily/141.环形链表.py
@@ -0,0 +1,41 @@
+'''
+给定一个链表，判断链表中是否有环。
+'''
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# python特性解法
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        s = set()
+        while head:
+            if head in s:
+                return True
+            else:
+                s.add(head)
+            head = head.next
+        return False
+
+# 快慢指针
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:return False
+        fast,low = head,head
+        while fast and fast.next:
+            fast = fast.next.next
+            low = low.next
+            if fast is low:
+                return True
+        return False
